fc41476e722aff450e133d9bef9a047eb530ce80
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-06-03 15:51:28 +0600

    after completing functions

---
+++ Functions/exercise-4.js
@@ -0,0 +1,29 @@
+function summation(a,b,c){
+    var sum=0
+    if (a==13){
+
+        return sum
+    }
+    else{
+        sum=sum+a
+    }
+    if (b==13){
+        return sum
+    }
+    else{
+        sum=sum+b
+    }
+    if (c==13){
+        return sum
+    }
+    else{
+        sum=sum+b
+    }
+
+    return sum
+
+}
+
+console.log(summation(1,2,3))
+console.log(summation(1,2,13))
+console.log(summation(1,13,3))
